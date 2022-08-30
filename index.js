3a66f51ae8d2ff45e033eab63d18b47780c6cfad
matisantacruzz <matias95ssc@gmail.com>
2022-08-30 19:19:02 -0300

    subida archivos html y js

---
+++ index.js
@@ -0,0 +1,24 @@
+// // Ejemplo Uno
+// let nombre = prompt("inserte su nombre");
+// let saludo = "Hola  " + nombre + ", bienvenido a mi sitio web";
+
+// console.log(saludo);
+
+// alert(saludo);
+
+// Ejemplo Dos con parceo
+// parceamos number asi el promt se convierte en numer
+
+// let numeroA = prompt("ingrese un numero del 1 al 10");
+// let numeroB = prompt("ingrese un segundo numero del 1 al 10");
+
+// let suma = Number(numeroA) + Number(numeroB);
+// console.log(suma);
+
+// alert(suma);
+
+
+
+if (18 == "18"){
+    console.log("Son iguales")
+}
