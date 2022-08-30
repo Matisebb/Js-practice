902608edcce6e4fb12b804344a9c92f2e9280a0a
matisantacruzz <matias95ssc@gmail.com>
2022-08-30 19:57:02 -0300

    actualizacion js promedio

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
-// // Ejemplo Uno
-// let nombre = prompt("inserte su nombre");
-// let saludo = "Hola  " + nombre + ", bienvenido a mi sitio web";
 
-// console.log(saludo);
+let nota1, nota2, nota3;
+nota1 = parseInt(prompt("Nota 1?"));
+nota2 = parseInt(prompt("Nota 2?"));
+nota3 = parseInt(prompt("Nota 3?"));
 
-// alert(saludo);
-
-// Ejemplo Dos con parceo
-// parceamos number asi el promt se convierte en numer
-
-// let numeroA = prompt("ingrese un numero del 1 al 10");
-// let numeroB = prompt("ingrese un segundo numero del 1 al 10");
-
-// let suma = Number(numeroA) + Number(numeroB);
-// console.log(suma);
-
-// alert(suma);
-
-
-
-if (18 == "18"){
-    console.log("Son iguales")
+let promedio = (nota1 + nota2 + nota3)/3;
+if (promedio >=8){
+    alert("Promocionado");
+} else if(promedio >= 4){
+    alert("Regular");
+} else {
+    alert("Desaprobado")
 }
+
+
