1dc5de6b4d9dd1b09d7db57b6b012117c706a238
matisantacruzz <matias95ssc@gmail.com>
2022-09-09 02:23:29 -0300

    actualizacion archivos html y js

--- index.js
+++ index.js
@@ -1,31 +1,71 @@
-let form = document.getElementById('form1');
-form.onsubmit = (e) => {
-    e.preventDefault();
-    let precio = parseFloat(document.getElementById('precio').value);
-    console.log(precio)
-    let porcentaje = parseFloat(document.getElementById('porcentaje').value);
-    console.log(porcentaje)
-    var limite = 10000;
-    let result = (precio*porcentaje/100);
-    console.log(result);
-    if (result < limite){
-        bonificacion = result;
-        pagar = precio - bonificacion;
-        console.log(pagar);
-    }else{
-        bonificacion = limite;
-        pagar = precio - bonificacion;
-        console.log(pagar);
+const productos = [
+    { nombre: "Cheese burger", precio: 600 },
+    { nombre: "American burger", precio: 800 },
+    { nombre: "Crispy burger", precio: 600 },
+    { nombre: "Five Guys", precio: 900 },
+    { nombre: "Shake Shack", precio: 800 },
+];
+
+let carrito = [];
+
+let seleccion = prompt("Hola bienvenidos a Saint burger, desea ordenar algo? \n Si \n No");
+
+while (seleccion != "Si" && seleccion != "No") {
+    alert("Por favor, ingresá Si o No");
+    seleccion = prompt("Hola, desea ordenar algo? \n Si \n No");
+}
+
+if (seleccion == "Si") {
+    alert("A continuacion el menu de Saint Burger:");
+    let todoslosProductos = productos.map((producto) => producto.nombre + " " + producto.precio + "$");
+    alert(todoslosProductos.join(" - "));
+} else if (seleccion == "No") {
+    alert("Gracias por elegirnos, vuelva pronto!");
+}
+
+while (seleccion != "No") {
+    let producto = prompt("Agrega un producto al carrito");
+    let precio = 0;
+
+    if (producto == "Cheese burger" || producto == "American burger" || producto == "Crispy burger" || producto == "Five Guys" || producto == "Shake Shack") {
+        switch (producto) {
+            case "Cheese burger":
+                precio = 600;
+                break;
+            case "American burger":
+                precio = 800;
+                break;
+            case "Crispy burger":
+                precio = 600;
+                break;
+            case "Five Guys":
+                precio = 900;
+                break;
+            case "Shake Shack":
+                precio = 800;
+                break;
+            default:
+                break;
+        }
+        let unidades = Number(prompt("Cuantas desea ordenar?"))
+
+        carrito.push({ producto, unidades, precio });
+        console.log(carrito);
+    } else {
+        alert("No tenemos ese producto");
     }
 
-    document.getElementById('myP').innerHTML = "DESCUENTO: " + bonificacion.toFixed(2); 
-    "<br>" 
-    document.getElementById('pym').innerHTML = "TOTAL A PAGAR: " + pagar.toFixed(2);
-} 
-function fun(){  
-    document.getElementById("form1").reset();
-    document.getElementById('myP').innerHTML = "INGRESE PRECIO Y PORCENTAJE DE DESCUENTO"
-    document.getElementById('pym').innerHTML = ""
+    seleccion = prompt("Desea seguir comprando?");
+}
 
-}  
 
+while (seleccion === "No") {
+    alert("Gracias por su compra! vuelva pronto!");
+    carrito.forEach((carritoFinal) => {
+        console.log(`producto: ${carritoFinal.producto}, unidades: ${carritoFinal.unidades}, total a pagar: ${carritoFinal.unidades * carritoFinal.precio}`)
+    })
+    break;
+}
+
+const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0);
+alert(`El total a pagar por su compra es de: ${total}`);
