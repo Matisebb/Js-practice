da090c07bcd3b8033da628ebdea3bce51efad7f2
matisantacruzz <matias95ssc@gmail.com>
2022-09-08 23:42:52 -0300

    actualizacion archivos html y js

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-let form = document.getElementById("form1");
-
+let form = document.getElementById('form1');
 form.onsubmit = (e) => {
     e.preventDefault();
-    let precio = parseFloat(document.getElementById("precio").value);
-    let porcentaje = parseFloat(document.getElementById("porcentaje").value);
+    let precio = parseFloat(document.getElementById('precio').value);
+    console.log(precio)
+    let porcentaje = parseFloat(document.getElementById('porcentaje').value);
+    console.log(porcentaje)
     var limite = 10000;
     let result = (precio*porcentaje/100);
+    console.log(result);
     if (result < limite){
         bonificacion = result;
-        pagar = porcentaje - bonificacion;
-    } else {
+        pagar = precio - bonificacion;
+        console.log(pagar);
+    }else{
         bonificacion = limite;
         pagar = precio - bonificacion;
+        console.log(pagar);
     }
 
-    document.getElementById("myP").innerHTML = "Descuento: " + bonificacion.toFixed(2);
-    "<br>"
-    document.getElementById("pym").innerHTML = "Total a pagar: " + pagar.toFixed(2);
-}
-
-function fun(){
+    document.getElementById('myP').innerHTML = "DESCUENTO: " + bonificacion.toFixed(2); 
+    "<br>" 
+    document.getElementById('pym').innerHTML = "TOTAL A PAGAR: " + pagar.toFixed(2);
+} 
+function fun(){  
     document.getElementById("form1").reset();
-    document.getElementById("myP").innerHTML = "Ingrese precio y porcentaje de descuento"
-    document.getElementById("pym").innerHTML = ""
-}
+    document.getElementById('myP').innerHTML = "INGRESE PRECIO Y PORCENTAJE DE DESCUENTO"
+    document.getElementById('pym').innerHTML = ""
+
+}  
+
