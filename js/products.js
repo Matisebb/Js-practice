14ec917c9b65addc1ccec3d5bac9c1bc00c22b92
matisantacruzz <matias95ssc@gmail.com>
2022-10-07 02:12:45 -0300

    modificacion html, js y css mas subida carpeta js

---
+++ js/products.js
@@ -0,0 +1,30 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Pizza Napoletana",
+        precio: 700,
+        img: "https://saboraitaliamx.com/wp-content/uploads/2021/11/shutterstock_1065375920.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 2,
+        nombre: "Pizza Margherita",
+        precio: 800,
+        img: "https://cdn.elcocinerocasero.com/imagen/receta/1000/2020-12-06-20-10-11/pizza-margarita.jpeg",
+        cantidad: 1,
+    },
+    {
+        id: 3,
+        nombre: "Pizza Pepperoni",
+        precio: 850,
+        img: "https://i.pinimg.com/originals/4b/2b/33/4b2b336eca9844c0b658ab2811a0f14b.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 4,
+        nombre: "Pizza Fugazzeta",
+        precio: 750,
+        img: "https://www.barilocheya.com.ar/pizzeriabase/wp-content/uploads/sites/6/2020/03/fugazeta.jpg",
+        cantidad: 1,
+    },
+];
